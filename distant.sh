a3059d49ace1219d163f941f5a5b07da2a53843f
Richard Massey <rwassey@gmail.com>
2016-07-25 10:52:42 -0400

    Second

--- distant.sh
+++ distant.sh
@@ -2,13 +2,15 @@
 
 # Config
 
+project_name=$PROJECT_NAME
+
 test_cmd=bin/rspec
 
 current_branch=$(git rev-parse --abbrev-ref HEAD)
 
 current_sha1=$(git rev-parse --verify HEAD)
 
-log_path=log/distant/$current_branch/$current_sha1
+log_path=log/distant/$project_name/$current_branch/$current_sha1
 
 logfile=$log_path/test.log
 
@@ -16,22 +18,38 @@ statusfile=$log_path/status
 
 timestamp=$log_path/timestamp
 
+function passing {
+  `cat $statusfile` == 0
+}
+
+function __status__ {
+  echo $([ "$passing" ] && echo 'passing' || echo 'failing')
+}
+
+function color {
+  echo $([ "$passing" ] && echo 'green' || echo 'red')
+}
+
+function shield {
+  echo '<img src="https://img.shields.io/badge/build-'$(__status__)'-'$(color)'.svg">'
+}
+
 
 
 # Ensure the expected directory structure is in place
 mkdir -p $log_path
 
 # Print the output of $test_cmd so we can show the output on failed builds
-$test_cmd 2>&1 | tee $logfile
+$test_cmd 2>&1 | tee $logfile &> /dev/null
 
 # Print the exit code of $test_cmd so we know if the build passed or failed
-echo ${PIPESTATUS[0]} 2>&1 | tee $statusfile
+echo ${PIPESTATUS[0]} > $statusfile
 
 # Print the date so we know which is the latest build
 # TODO use `date` instead of `ruby`
 echo $(ruby -e 'puts Time.now.utc.to_i') > $timestamp
 
 # Put the files on s3 so they can be read via ajax in the PR
-aws s3 sync ./log/distant/ s3://distant/
+aws s3 sync ./log/distant/$project_name/ s3://distant/$project_name/ &> /dev/null
 
-# TODO echo GITHUB_MARKDOWN_BUILD_STATUS_BADGE_WITH_LINK_TO_VIEW_ON_S3
+shield
